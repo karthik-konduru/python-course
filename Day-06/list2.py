9fd43a19ddd66887e0ee56a2e4b7d716b39f760e
Karthikkonduru <karthikkonduru148@gmail.com>
2026-09-07 14:48:40 +0530

    List Operations.

---
+++ Day-06/list2.py
@@ -0,0 +1,6 @@
+#List Operations.
+l1=[1,2,3]
+l2=[4,5,6]
+print(" adding list1 and list2:",l1+l2)# we can use operators.
+print("multplication of list1 and list2:",l1*3)# we are printg the list multiple times
+print(l2*5)
