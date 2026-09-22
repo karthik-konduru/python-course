f5b6e1abdaaacf853094c6c19d1e4237d246a8ed
Karthikkonduru <karthikkonduru148@gmail.com>
2026-09-22 17:17:33 +0530

    introduction to OPPs

---
+++ Day-23/oops.py
@@ -0,0 +1,16 @@
+class PFS_66:
+    def __init__(self,name,roll_number,marks):
+        self.name=name 
+        self.roll_number=roll_number
+        self.marks = marks
+        pass
+    def exams(self):
+        print("grand test")
+    def attend_classes(Self):
+        print("assignments")
+    def python_weekly_exam (self):
+        print("python test")
+student1 = PFS_66("karthik","WER222",1000)
+student1.exams()
+student1.python_weekly_exam()
+
